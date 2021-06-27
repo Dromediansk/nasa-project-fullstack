fd0481887f7f560b73270e5dc5e4eea6e8c3d6ba
Dromediansk <miroslav.pillar@senacor.com>
2021-06-27 19:15:21 +0200

    POST and DELETE launches endpoints

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,11 +1,13 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
-  date: new Date("December 27, 2030"),
-  destination: "Kepler-442 b",
+  launchDate: new Date("December 27, 2030"),
+  target: "Kepler-442 b",
   customers: ["NASA", "ZTM"],
   upcoming: true,
   success: true,
@@ -13,6 +15,37 @@ const launch = {
 
 launches.set(launch.flightNumber, launch);
 
+function existsLaunchById(launchId) {
+  return launches.has(launchId);
+}
+
+function getAllLaunches() {
+  return Array.from(launches.values());
+}
+
+function addNewLaunch(newLaunch) {
+  latestFlightNumber += 1;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(newLaunch, {
+      success: true,
+      upcoming: true,
+      customers: ["ZTM", "NASA"],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
+}
+
 module.exports = {
-  launches,
+  existsLaunchById,
+  getAllLaunches,
+  addNewLaunch,
+  abortLaunchById,
 };
