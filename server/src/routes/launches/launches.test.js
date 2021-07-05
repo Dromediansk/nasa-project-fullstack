2470354af79c9c51918fcd897e5ec80fd018b99a
Dromediansk <miroslav.pillar@senacor.com>
2021-07-05 06:57:03 +0200

    versioning api

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -11,16 +11,16 @@ describe("Launches API", () => {
     await mongoDisconnect();
   });
 
-  describe("GET /launches", () => {
+  describe("GET /v1/launches", () => {
     it("should respond with 200 success", async () => {
       const response = await request(app)
-        .get("/launches")
+        .get("/v1/launches")
         .expect("Content-Type", /json/)
         .expect(200);
     });
   });
 
-  describe("POST /launches", () => {
+  describe("POST /v1/launches", () => {
     const completeLaunchData = {
       mission: "USS Enterprise",
       rocket: "Test rocket",
@@ -43,7 +43,7 @@ describe("Launches API", () => {
 
     it("should response with 201 created", async () => {
       const response = await request(app)
-        .post("/launches")
+        .post("/v1/launches")
         .send(completeLaunchData)
         .expect("Content-Type", /json/)
         .expect(201);
@@ -57,7 +57,7 @@ describe("Launches API", () => {
 
     it("should catch missing required properties", async () => {
       const response = await request(app)
-        .post("/launches")
+        .post("/v1/launches")
         .send(launchDataWithoutDate)
         .expect("Content-Type", /json/)
         .expect(400);
@@ -69,7 +69,7 @@ describe("Launches API", () => {
 
     it("should catch invalid launchDate", async () => {
       const response = await request(app)
-        .post("/launches")
+        .post("/v1/launches")
         .send(launchDataWithInvalidDate)
         .expect("Content-Type", /json/)
         .expect(400);
